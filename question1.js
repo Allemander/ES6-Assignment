36d53263b8e9ed49b710db33499008db22ea0a16
Allemander <matthewaminyard@gmail.com>
2022-03-15 12:01:24 -0400

    split

---
+++ question1.js
@@ -0,0 +1,10 @@
+class Employee {
+    constructor(name, IdNumber, Permissions, StoreNumber) {
+      this.name = name;
+      this.IdNumber = IdNumber;
+      this.Permissions = Permissions;
+      this.StoreNumber = StoreNumber;
+    }
+  }
+
+  
